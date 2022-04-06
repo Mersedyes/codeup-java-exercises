0d100ffc02492e532bb0dcfd67cf2c0d3b211f4d
Mersedyes Livingston <mersedyes.livingston@gmail.com>
2022-04-06 16:46:08 -0500

    completed ConsoleIO explore the scanner class activity

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -6,5 +6,19 @@ public class ConsoleExercises {
         double pi = 3.14159;
         System.out.printf("The value of pi is approximate %.2f\n", pi);
 
+        System.out.println("Hey user, enter an integer please:");
+
+        int userInt = scanner.nextInt();
+
+        System.out.println("Hey user, enter three words one at a time:");
+        String firstWord = scanner.next();
+        String secondWord = scanner.next();
+        String thirdWord = scanner.next();
+        System.out.printf("Your words are: ",firstWord,secondWord,thirdWord);
+
+        String userSentence = scanner.nextLine();
+        System.out.println("Enter a sentence:");
+        System.out.println(userSentence);
+
     }
 }
