5ceb0597b52b62312d1cced85724e8fa91927c28
Mersedyes Livingston <mersedyes.livingston@gmail.com>
2022-04-06 16:32:28 -0500

    added ConsoleExercises.java

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        double pi = 3.14159;
+        System.out.printf("The value of pi is approximate %.2f\n", pi);
+
+    }
+}
