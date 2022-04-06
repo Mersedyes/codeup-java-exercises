621263145b616ae8f3078984ced8c2ed50ec503d
Mersedyes Livingston <mersedyes.livingston@gmail.com>
2022-04-06 14:37:07 -0500

    completed intro to java assignment

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,52 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+//        int myFavoriteNumber = 11;
+//        System.out.println(myFavoriteNumber);
+//
+//        String myString = "look a string";
+//        System.out.println(myString);
+
+        // myString = "R";
+
+        //Long myNumber = 3.14;
+        //myNumber = 3.14;
+        //System.out.println(myNumber);
+
+//        myNumber = 123L;
+//        float myNumber;
+//        myNumber = (float) 3.14;
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String class = "String";
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+        //this had a runtime error, it started running until it got to this point
+
+//        int three = (int) "three";
+        //can't attach a string to an int
+
+//        int x = 4;
+//        x = x + 5;
+//        System.out.println();
+//
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+//
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
     }
 }
