393ee34297c5a679bd58b9294cab12f5af017c55
Mersedyes Livingston <mersedyes.livingston@gmail.com>
2022-04-09 22:45:46 -0500

    updated

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -2,6 +2,13 @@ import java.util.Scanner;
 
 public class MethodsExercises {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        //Scanner sc = new Scanner(System.in);
+            int ten = 10;
+            int two = 2;
+            System.out.print("Addition of given numbers = ");
+            System.out.println(ten + two);
+            System.out.println(ten - two);
+            System.out.println(ten * two);
+            System.out.println(ten % two);
     }
 }
