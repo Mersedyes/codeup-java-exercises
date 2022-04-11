dc931b8e5e3b1c97895fc49522b6395f0d47ab94
Mersedyes Livingston <mersedyes.livingston@gmail.com>
2022-04-11 08:18:18 -0500

    finished second challenge in assignment

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.lang.Math;
 
 public class MethodsExercises {
     public static void main(String[] args) {
@@ -42,4 +43,19 @@ public class MethodsExercises {
     }
 
     //Bonus: try recursion to get the method to call itself
+
+    //Create a method that validates that user input is in a certain range
+    public static int getInteger(int min, int max){
+        Scanner sc = new Scanner(System.in);
+            System.out.print("Enter a number between 1 and 10: ");
+
+            int userInput = Integer.parseInt(sc.nextLine());
+            if (userInput < min || userInput > max){
+                System.out.println("This number is not in range. Please try again by entering a number between 1 and 10.");
+                return getInteger(min, max);
+            } else {
+                System.out.println("You number is within range. Nice job!");
+                return userInput;
+            }
+    }
 }
