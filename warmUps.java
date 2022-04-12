3f4988cae1009ca88801d0f540398bb47dd03859
Mersedyes Livingston <mersedyes.livingston@gmail.com>
2022-04-12 09:37:32 -0500

    added challenge for today

--- warmUps.java
+++ warmUps.java
@@ -5,21 +5,41 @@ public class warmUps {
                 hasTransportation
                 canComeInPerson (the value you get will be a boolean expression using the previous two)
         Write an if statement that prints a message stating whether or not someone can come in person.*/
-            boolean livesInCity = false;
-            boolean hasTransportation = true;
-            boolean canComeInPerson = livesInCity || hasTransportation;
-
-                    if(canComeInPerson){
-                        System.out.println("You are able to come in person");
-                    } else {
-                        System.out.println("You may want to consider virtual options")
-                    }
+//            boolean livesInCity = false;
+//            boolean hasTransportation = true;
+//            boolean canComeInPerson = livesInCity || hasTransportation;
+//
+//                    if(canComeInPerson){
+//                        System.out.println("You are able to come in person");
+//                    } else {
+//                        System.out.println("You may want to consider virtual options")
+//                    }
 
 //            if (livesInCity = true || livesInCity = false && hasTransportation = true){
 //                System.out.println("You are able to come in person!");
 //            } else {
 //                System.out.println("You may want to consider going virtual options")
 //            }
-
+        //challenge 4.12.22
+//        public class SpousePojo {
+//
+//            public String firstName;
+//            public String lastName;
+//            private LocalDate marriageDate;
+//
+//            public EmployeePojo(String firstName, String lastName, LocalDate marriageDate) {
+//                this.firstName = firstName;
+//                this.lastName = lastName;
+//                this.marriageDate = marriageDate;
+//            }
+//
+//            public String name() {
+//                return this.firstName + " " + this.lastName;
+//            }
+//
+//            public LocalDate getStart() {
+//                return this.marriageDate;
+//            }
+//        }
     }
     }
